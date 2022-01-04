24627b1649d9e98865d190935f108e6241cb0cbb
Alan Boglioli <alan.boglioli@gmail.com>
2022-01-04 01:21:29 -0300

    feature: basic schema props validation for config

--- domain/schema/schema.go
+++ domain/schema/schema.go
@@ -56,52 +56,8 @@ func (s *Schema) Props() map[string]props.Prop {
 
 func (s *Schema) Validate(c config.ConfigData) error {
 	for k, v := range c {
-		prop, ok := s.props[k]
-		if !ok {
-			return fmt.Errorf("prop %s not found in schema", k)
-		}
-
-		if obj, ok := v.(map[string]interface{}); ok {
-			if err := s.Validate(obj); err != nil {
-				return fmt.Errorf("object %s: %s", k, err.Error())
-			}
-		}
-
-		switch v.(type) {
-		case string:
-			if prop.Type() != props.STRING {
-				return fmt.Errorf("%v is not a string", v)
-			}
-		case int:
-			if prop.Type() != props.INT {
-				return fmt.Errorf("%v is not an integer", v)
-			}
-		case int32:
-			if prop.Type() != props.INT {
-				return fmt.Errorf("%v is not an integer", v)
-			}
-		case int64:
-			if prop.Type() != props.INT {
-				return fmt.Errorf("%v is not an integer", v)
-			}
-		case float32:
-			if prop.Type() != props.FLOAT {
-				return fmt.Errorf("%v is not a float", v)
-			}
-		case float64:
-			if prop.Type() != props.FLOAT {
-				return fmt.Errorf("%v is not a float", v)
-			}
-		case bool:
-			if prop.Type() != props.BOOL {
-				return fmt.Errorf("%v is not a boolean", v)
-			}
-		case map[string]interface{}:
-			if prop.Type() != props.OBJECT {
-				return fmt.Errorf("%v is not an object", v)
-			}
-		default:
-			return fmt.Errorf("%v unknown type", v)
+		if err := validateConfig(k, v, s.props); err != nil {
+			return err
 		}
 	}
 
@@ -116,4 +72,148 @@ func (s *Schema) MarshalJSON() ([]byte, error) {
 	}
 
 	return json.Marshal(&d)
+}
+
+func validateConfig(key string, value interface{}, propsMap map[string]props.Prop) error {
+	prop, ok := propsMap[key]
+	if !ok {
+		return fmt.Errorf("prop %s not found in schema", key)
+	}
+
+	if value == nil && prop.Default() != nil {
+		value = prop.Default()
+	}
+
+	if value == nil && prop.Required() {
+		return fmt.Errorf("value for %s is required", key)
+	}
+
+	if len(prop.Enum()) > 0 {
+		isFromEnum := false
+		for _, e := range prop.Enum() {
+			if value == e {
+				isFromEnum = true
+				break
+			}
+		}
+
+		if !isFromEnum {
+			return fmt.Errorf("%s:%v is not in enum values", key, value)
+		}
+	}
+
+	switch value := value.(type) {
+	case string:
+		if prop.Type() != props.STRING {
+			return fmt.Errorf("%v is not a string", value)
+		}
+	case int:
+		if prop.Type() != props.INT {
+			return fmt.Errorf("%s:%v is not an integer", key, value)
+		}
+
+		if prop.Interval() != nil {
+			interval := prop.Interval()
+
+			if value < int(interval.Min()) {
+				return fmt.Errorf("%s:%v is lesser than the minimum value in interval", key, value)
+			}
+
+			if value > int(interval.Max()) {
+				return fmt.Errorf("%s:%v is greater than the maximum value in interval", key, value)
+			}
+		}
+	case int32:
+		if prop.Type() != props.INT {
+			return fmt.Errorf("%s:%v is not an integer", key, value)
+		}
+
+		if prop.Interval() != nil {
+			interval := prop.Interval()
+
+			if value < int32(interval.Min()) {
+				return fmt.Errorf("%s:%v is lesser than the minimum value in interval", key, value)
+			}
+
+			if value > int32(interval.Max()) {
+				return fmt.Errorf("%s:%v is greater than the maximum value in interval", key, value)
+			}
+		}
+	case int64:
+		if prop.Type() != props.INT {
+			return fmt.Errorf("%s:%v is not an integer", key, value)
+		}
+
+		if prop.Interval() != nil {
+			interval := prop.Interval()
+
+			if value < int64(interval.Min()) {
+				return fmt.Errorf("%s:%v is lesser than the minimum value in interval", key, value)
+			}
+
+			if value > int64(interval.Max()) {
+				return fmt.Errorf("%s:%v is greater than the maximum value in interval", key, value)
+			}
+		}
+	case float32:
+		if prop.Type() != props.FLOAT {
+			return fmt.Errorf("%s:%v is not a float", key, value)
+		}
+
+		if prop.Interval() != nil {
+			interval := prop.Interval()
+
+			if value < float32(interval.Min()) {
+				return fmt.Errorf("%s:%v is lesser than the minimum value in interval", key, value)
+			}
+
+			if value > float32(interval.Max()) {
+				return fmt.Errorf("%s:%v is greater than the maximum value in interval", key, value)
+			}
+		}
+	case float64:
+		if prop.Type() != props.FLOAT {
+			return fmt.Errorf("%s:%v is not a float", key, value)
+		}
+
+		if prop.Interval() != nil {
+			interval := prop.Interval()
+
+			if value < interval.Min() {
+				return fmt.Errorf("%s:%v is lesser than the minimum value in interval", key, value)
+			}
+
+			if value > interval.Max() {
+				return fmt.Errorf("%s:%v is greater than the maximum value in interval", key, value)
+			}
+		}
+	case bool:
+		if prop.Type() != props.BOOL {
+			return fmt.Errorf("%s:%v is not a boolean", key, value)
+		}
+	case map[string]interface{}:
+		if prop.Type() != props.OBJECT {
+			return fmt.Errorf("%s:%v is not an object", key, value)
+		}
+
+		for k, v := range value {
+			if err := validateConfig(k, v, prop.Props()); err != nil {
+				return fmt.Errorf("object %s: %s", key, err.Error())
+			}
+		}
+	case []interface{}:
+		if !prop.IsArray() {
+			return fmt.Errorf("%s is not an array", key)
+		}
+
+		for i, v := range value {
+			if err := validateConfig(key, v, propsMap); err != nil {
+				return fmt.Errorf("array item %d: %s", i, err.Error())
+			}
+		}
+	default:
+		return fmt.Errorf("%v unknown type in %s", value, key)
+	}
+
+	return nil
 }
--- domain/schema/schema_test.go
+++ domain/schema/schema_test.go
@@ -39,7 +39,7 @@ func TestValidateSchema(t *testing.T) {
 			err: true,
 		},
 		{
-			name: "invalid object",
+			name: "invalid object containing value",
 			schema: func(t *test) *Schema {
 				str, err := props.NewString("str")
 				utils.Ok(err)
@@ -60,6 +60,137 @@ func TestValidateSchema(t *testing.T) {
 				},
 			},
 			err: true,
+		},
+		{
+			name: "invalid interval",
+			schema: func(t *test) *Schema {
+				integer, err := props.NewInteger("integer", props.WithInterval(4, 6))
+				utils.Ok(err)
+
+				obj, err := props.NewObject("obj", props.WithProps(integer))
+				utils.Ok(err)
+
+				n, err := NewName(t.name)
+				utils.Ok(err)
+
+				s, err := NewSchema(n, obj)
+
+				return s
+			},
+			config: config.ConfigData{
+				"obj": map[string]interface{}{
+					"integer": 3,
+				},
+			},
+			err: true,
+		},
+		{
+			name: "invalid enum",
+			schema: func(t *test) *Schema {
+				env, err := props.NewString("env", props.WithEnum("dev", "prod"))
+				utils.Ok(err)
+
+				n, err := NewName(t.name)
+				utils.Ok(err)
+
+				s, err := NewSchema(n, env)
+
+				return s
+			},
+			config: config.ConfigData{
+				"env": "staging",
+			},
+			err: true,
+		},
+		{
+			name: "invalid array",
+			schema: func(t *test) *Schema {
+				strs, err := props.NewString("strs", props.WithArray(), props.WithEnum("dev", "prod"))
+				utils.Ok(err)
+
+				n, err := NewName(t.name)
+				utils.Ok(err)
+
+				s, err := NewSchema(n, strs)
+
+				return s
+			},
+			config: config.ConfigData{
+				"strs": "prod",
+			},
+			err: true,
+		},
+		{
+			name: "valid enum",
+			schema: func(t *test) *Schema {
+				env, err := props.NewString("env", props.WithEnum("dev", "prod"))
+				utils.Ok(err)
+
+				n, err := NewName(t.name)
+				utils.Ok(err)
+
+				s, err := NewSchema(n, env)
+
+				return s
+			},
+			config: config.ConfigData{
+				"env": "prod",
+			},
+			err: false,
+		},
+		{
+			name: "valid object",
+			schema: func(t *test) *Schema {
+				integer, err := props.NewInteger("integer", props.WithInterval(4, 6))
+				utils.Ok(err)
+
+				str, err := props.NewString("str", props.WithRequired(true))
+				utils.Ok(err)
+
+				obj, err := props.NewObject("obj", props.WithProps(integer, str))
+				utils.Ok(err)
+
+				n, err := NewName(t.name)
+				utils.Ok(err)
+
+				s, err := NewSchema(n, obj)
+
+				return s
+			},
+			config: config.ConfigData{
+				"obj": map[string]interface{}{
+					"integer": 5,
+					"str":     "hello",
+				},
+			},
+			err: false,
+		},
+		{
+			name: "valid array",
+			schema: func(t *test) *Schema {
+				integer, err := props.NewInteger(
+					"integers",
+					props.WithArray(),
+					props.WithInterval(2, 6),
+				)
+				utils.Ok(err)
+
+				obj, err := props.NewObject("obj", props.WithProps(integer))
+				utils.Ok(err)
+
+				n, err := NewName(t.name)
+				utils.Ok(err)
+
+				s, err := NewSchema(n, obj)
+
+				return s
+			},
+			config: config.ConfigData{
+				"obj": map[string]interface{}{
+					"integers": []interface{}{2, 3, 4, 5, 6},
+				},
+			},
+			err: false,
 		},
 	}
 
